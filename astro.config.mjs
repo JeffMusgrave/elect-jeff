2b434cbba75d05ae178e04a61a0b3b52500ad9e8
Jeff <jeff.musgrave@gmail.com>
2022-10-15 22:54:07 -0400

    added robots and sitemap plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+import robots from "astro-robots-txt"
+
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://www.electjeff.ca',
+  integrations: [tailwind(), sitemap(), robots()]
 });
